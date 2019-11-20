d7dc770fc0ecd766845cfd6cb0668daa97cc5fe4
Haejoon Lee <haejoon309@naver.com>
2019-11-20 13:32:59 +0900

    add test files

---
+++ src/test_operators.py
@@ -0,0 +1,11 @@
+import operators
+
+def test_add():
+    a = 10
+    b = 20
+    assert operators.add(10, 20) == 30
+
+def test_sub():
+    a = 10
+    b = 20
+    assert operators.sub(10, 20) == -10
